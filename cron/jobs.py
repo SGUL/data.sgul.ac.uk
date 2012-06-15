d8a440e9b441ee4671ea1eabb74f2440ea0606e6
Giuseppe Sollazzo <puntofisso@gmail.com>
2012-06-15 16:37:10 +0100

    Jobs list downloaded

--- cron/jobs.py
+++ cron/jobs.py
@@ -5,17 +5,37 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 
 
+def downloadJobDetails(url):
+    jobs_detail_url = settings["jobs"]["url"]  + "/" + url
+    return jobs_detail_url
+
 
 #read settings, load url, parse resulting text
 settings_text = open("config.json", "r").read()
 settings = json.loads(settings_text)
-jobs_url = settings["jobs"]["url"]
+jobs_url = settings["jobs"]["url"] + "/" + settings["jobs"]["list"]
 
 
 # get page and soupify
 jobs = (urllib2.urlopen(jobs_url)).read()
 soup = BeautifulSoup(jobs)
+printHide_element = soup.findAll(attrs={'class': 'printHide'})[0]
+soup_div = BeautifulSoup(str(printHide_element))
 
-for i in soup.findAll(attrs={'class': 'printHide'}):
-    print i.extract()
+type = ""
+topic = ""
 
+# navigate in each element of the page
+for elem in soup_div.findAll():
+    tagname = elem.name
+
+    if tagname == "h2":
+        type = elem.contents[0]
+    elif tagname == "strong":
+        topic = elem.contents[0]
+    elif tagname == "li":
+        href = elem.a['href']
+        title = elem.a['title']
+        # Now (type,topic,href,title) is a description of the job
+	# ready to download further information
+        jobs_data = downloadJobDetails(href)
