f0e70fc4f58aa8fb4605ecc25c09676ef86bd597
Giuseppe Sollazzo <puntofisso@gmail.com>
2012-08-02 16:38:04 +0100

    Errors - Xpath should search in current node

--- cron/cris.py
+++ cron/cris.py
@@ -16,21 +16,45 @@ def parseUser(xmlFile):
         href = user.get("href")
         type = user.get("type-id")
         current = e('//api:is-current-staff')[0].text
-        try:
-            title = e('//api:title')[0].text
-        except Exception, err:
-            title = ""
-        lastname = e('//api:last-name')[0].text
-        firstname = e('//api:first-name')[0].text
-        email = e('//api:email-address')[0].text
-        print prop_id, ",", id, ",", username, ",", href, ",", type, ",", current, ",", title, ",", lastname, ",", firstname, ",", email
+        currents = e('//api:is-current-staff')
+        # TODO this is all wrong - it needs to search in the current node
+        for c in currents:
+            print c.text
+        #try:
+        #    title = e('//api:title')[0].text
+        #except Exception, err:
+        #    title = ""
+        #lastname = e('//api:last-name')[0].text
+        #firstname = e('//api:first-name')[0].text
+        #email = e('//api:email-address')[0].text
+        #print prop_id, ",", id, ",", username, ",", href, ",", type, ",", current, ",", title, ",", lastname, ",", firstname, ",", email
 
 def parseUserFeed(xmlFile):
     pass
-    # TODO do something when Symplectic confirms this is available   
 
+def parseUserFeedList(xmlFile):
+    tree = etree.parse(xmlFile)
+    e = etree.XPathEvaluator(tree)
+    e.register_namespace('api', 'http://www.symplectic.co.uk/publications/api')
+    # get all users in this page
+    users = e('//api:user-feed-entry/api:last-name')
+    for user in users:
+        print user.text
+    #for user in users:
+    #    prop_id = user.get("proprietary-id")
+    #    id = user.get("id")
+    #    username = user.get("username")
+    #    href = user.get("href")
+    #    parseUser(href)
 
-def parseList(xmlFile):
+    # get next/last     
+    next = e('//api:page[@position="next"]')[0].get("href")
+    last = e('//api:page[@position="last"]')[0].get("href")
+    #if next <> last:
+    #    parseUserFeedList(next)
+    
+
+def parseUserList(xmlFile):
     tree = etree.parse(xmlFile)
     e = etree.XPathEvaluator(tree)
     e.register_namespace('api', 'http://www.symplectic.co.uk/publications/api')
@@ -46,8 +70,12 @@ def parseList(xmlFile):
     # get next/last     
     next = e('//api:page[@position="next"]')[0].get("href")
     last = e('//api:page[@position="last"]')[0].get("href")
-    if next <> last:
-        parseList(next)
+    #if next <> last:
+    #    parseUserList(next)
+
+
+def parsePublicationList(xmlFile):
+    pass
 
 #read settings, load url, parse resulting text
 settings_text = open("config.json", "r").read()
@@ -55,4 +83,5 @@ settings = json.loads(settings_text)
 cris_url = settings["cris"]["url"]
 cris_port = settings["cris"]["port"]
 
-parseList(cris_url + ":" + cris_port +"/publications-api/objects?categories=users")
+parseUserList(cris_url + ":" + cris_port +"/publications-api/objects?categories=users")
+#parseUserFeedList(cris_url + ":" + cris_port +"/publications-api/user-feeds/STAFF")
