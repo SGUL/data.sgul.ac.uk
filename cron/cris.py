b20420a2afbf6b667978e05f4342261170fda039
Giuseppe Sollazzo <puntofisso@gmail.com>
2012-08-03 17:10:58 +0100

    Fixed authors extract and produced a draft inCites dataset

--- cron/cris.py
+++ cron/cris.py
@@ -13,51 +13,76 @@ def parseUser(xmlFile):
         prop_id = user.get("proprietary-id")
         id = user.get("id")
         username = user.get("username")
+
         href = user.get("href")
         type = user.get("type-id")
         current = e('//api:is-current-staff')[0].text
-        currents = e('//api:is-current-staff')
-        # TODO this is all wrong - it needs to search in the current node
-        for c in currents:
-            print c.text
-        #try:
-        #    title = e('//api:title')[0].text
-        #except Exception, err:
-        #    title = ""
-        #lastname = e('//api:last-name')[0].text
-        #firstname = e('//api:first-name')[0].text
-        #email = e('//api:email-address')[0].text
-        #print prop_id, ",", id, ",", username, ",", href, ",", type, ",", current, ",", title, ",", lastname, ",", firstname, ",", email
+       
+        try:
+            title = e('//api:title')[0].text
+        except Exception, err:
+            title = ""
+
+        lastname = e('//api:last-name')[0].text
+        firstname = e('//api:first-name')[0].text
+        email = e('//api:email-address')[0].text
+        result = []
+        if prop_id <> None:
+            hr_feed_url = (cris_url + ":" + cris_port +"/publications-api/user-feed/users/"+prop_id)
+            result = parseUserFeed(hr_feed_url)
+            finalres = [prop_id, lastname, firstname, email, result.pop(0), result.pop(0), result.pop(0), result.pop(0), result.pop(0), result.pop(0), result.pop(0), result.pop(0), result.pop(0), result.pop(0)]
+            do_inCites_authors(finalres)
+
 
 def parseUserFeed(xmlFile):
-    pass
-
-def parseUserFeedList(xmlFile):
+    returnlist = []
     tree = etree.parse(xmlFile)
     e = etree.XPathEvaluator(tree)
     e.register_namespace('api', 'http://www.symplectic.co.uk/publications/api')
     # get all users in this page
-    users = e('//api:user-feed-entry')
-    for user in users:
-        enew = etree.XPathEvaluator(user)
-        enew.register_namespace('api', 'http://www.symplectic.co.uk/publications/api')
-        last = enew('api:last-name')
-        for l in last:
-            print l.text
-            # TODO Wrong: it's got all the users
-        #user.e('//api:last-name')
-        #prop_id = user.get("proprietary-id")
-        #id = user.get("id")
-        #username = user.get("username")
-        #href = user.get("href")
-
-    # get next/last     
-    next = e('//api:page[@position="next"]')[0].get("href")
-    last = e('//api:page[@position="last"]')[0].get("href")
-    #if next <> last:
-        #parseUserFeedList(next)
+    try:
+        primarygroup = e('//api:primary-group-descriptor')[0].text
+    except Exception, err:
+        primarygroup = ""
+    try:
+        isacademic = e('//api:is-academic')[0].text
+    except Exception, err:
+        isacademic = ""
+    try:
+        arrive_date = e('//api:arrive-date')[0].text
+    except Exception, err:
+        arrive_date = ""
+    try:
+        leave_date = e('//api:leave-date')[0].text
+    except Exception, err:
+        leave_date = ""
+    try:
+        dateofbirth = e('//api:generic-field-03')[0].text
+    except Exception, err:
+        dateofbirth = ""
+    try:
+        ou_1 = e('//api:generic-field-04')[0].text
+    except Exception, err:
+        ou_1 = ""
+    try:
+        ou_2 = e('//api:generic-field-05')[0].text
+    except Exception, err:
+        ou_2 = ""
+    try:
+        jobtitle = e('//api:generic-field-06')[0].text
+    except Exception, err:
+        jobtitle = ""
+    try:
+        ft = e('//api:generic-field-07')[0].text
+    except Exception, err:
+        ft = ""
+    try:
+        perm_ft_text = e('//api:generic-field-15')[0].text
+    except Exception, err:
+        perm_ft_text = ""
+    returnlist = [primarygroup, isacademic, arrive_date, leave_date, dateofbirth, ou_1, ou_2, jobtitle, ft, perm_ft_text]
+    return returnlist
     
-
 def parseUserList(xmlFile):
     tree = etree.parse(xmlFile)
     e = etree.XPathEvaluator(tree)
@@ -74,9 +99,28 @@ def parseUserList(xmlFile):
     # get next/last     
     next = e('//api:page[@position="next"]')[0].get("href")
     last = e('//api:page[@position="last"]')[0].get("href")
-    #if next <> last:
-    #    parseUserList(next)
+    if next <> last:
+        parseUserList(next)
 
+# prints author export for in_cites
+def do_inCites_authors(list):
+    #[prop_id, lastname, firstname, email,primarygroup, isacademic, arrive_date, leave_date, dateofbirth, ou_1, ou_2, jobtitle, ft, perm_ft_text]
+    # not the smartest code ever...
+    prop_id = list.pop(0)
+    lastname = list.pop(0)
+    firstname = list.pop(0)
+    email = list.pop(0)
+    primarygroup = list.pop(0)
+    isacademic = list.pop(0)
+    arrive_date = list.pop(0)
+    leave_date = list.pop(0)
+    dateofbirth = list.pop(0)
+    ou_1 = list.pop(0)
+    ou_2 = list.pop(0)
+    jobtitle = list.pop(0)
+    ft = list.pop(0)
+    perm_ft_text = list.pop(0)
+    print prop_id + "," + lastname + "," + firstname + "," + email + '"St. George\'s, University of London"' + "," + "Cranmer Terrace" + "," + "London" + "," + "United Kingdom" + "," + "SW17 0RE" + "," + ou_1 + "," + arrive_date[0:4] + "," + leave_date[0:4]
 
 def parsePublicationList(xmlFile):
     pass
@@ -87,5 +131,4 @@ settings = json.loads(settings_text)
 cris_url = settings["cris"]["url"]
 cris_port = settings["cris"]["port"]
 
-#parseUserList(cris_url + ":" + cris_port +"/publications-api/objects?categories=users")
-parseUserFeedList(cris_url + ":" + cris_port +"/publications-api/user-feeds/STAFF")
+parseUserList(cris_url + ":" + cris_port +"/publications-api/objects?categories=users")
