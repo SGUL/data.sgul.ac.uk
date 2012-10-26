46e9354e5c1426c3f885247a75caa5c9c3e8bddd
Giuseppe Sollazzo <puntofisso@gmail.com>
2012-10-26 18:40:05 +0100

    Starting to use xpath

--- cron/cris.py
+++ cron/cris.py
@@ -127,124 +127,130 @@ def do_inCites_authors(list):
         print symp_id + "," + lastname + "," + firstname + "," + email + ',"St. George\'s, University of London",' + primarygroup + "," + "Cranmer Terrace" + "," + "London" + "," + "United Kingdom" + "," + "SW17 0RE" + "," + ou_1 + "," + arrive_date[0:4] + "," + leave_date[0:4]
         print symp_id + "," + lastname + "," + firstname + "," + email + ',"St. George\'s, University of London",' + primarygroup + "," + "Cranmer Terrace" + "," + "London" + "," + "United Kingdom" + "," + "SW17 0RE" + "," + ou_2 + "," + arrive_date[0:4] + "," + leave_date[0:4]
 
+
+
+
+
 # Publications management
 def parsePublicationList(xmlFile):
     tree = etree.parse(xmlFile)
+    e = etree.XPathEvaluator(tree, namespaces={'def':'http://www.w3.org/2005/Atom'})
+    #e.register_namespace('def','http://www.w3.org/2005/Atom')
+    e.register_namespace('api', 'http://www.symplectic.co.uk/publications/api')
 
-    file = urllib2.urlopen(xmlFile)
-    data = file.read()
-    file.close()
-    dom = parseString(data)
 
-    entries = dom.getElementsByTagName('entry')
+   # file = urllib2.urlopen(xmlFile)
+   # data = file.read()
+   # file.close()
+   # dom = parseString(data)
 
+    #entries = dom.getElementsByTagName('entry')
+
+    #for entry in entries:
+     #   pub_id = (entry.getElementsByTagName('id'))[0].childNodes[0].data
+     #   title = (entry.getElementsByTagName('title'))[0].childNodes[0].data
+     #   links = (entry.getElementsByTagName('link'))
+    #   for link in links:
+    #        rel = link.getAttribute('rel')
+    #        if rel == 'alternate':
+    #            pub_url = link.getAttribute('href')
+    #        elif rel == 'related' and link.getAttribute('title') == 'Relationships':
+    #            pub_rel_url = link.getAttribute('href')
+
+    #    print pub_url
+    #publicationData = parsePublicationUrl(pub_url) #).encode('ascii','xmlcharrefreplace')
+
+    entries = e('//def:entry')
     for entry in entries:
-        pub_id = (entry.getElementsByTagName('id'))[0].childNodes[0].data 
-        title = (entry.getElementsByTagName('title'))[0].childNodes[0].data 
-        links = (entry.getElementsByTagName('link'))
-	for link in links:
-            rel = link.getAttribute('rel')
-            if rel == 'alternate':
-	        pub_url = link.getAttribute('href')
-            elif rel == 'related' and link.getAttribute('title') == 'Relationships':
-                pub_rel_url = link.getAttribute('href')
-
+        #print (entry.findall('*'))[0].text
+        mypath = tree.getpath(entry) + "/category"
+	print mypath
+	category = e(mypath)
+        #print category
         
-
-        # title
-        # source, startPage, year
-        pub_info = parsePublicationUrl(pub_url).encode('ascii','xmlcharrefreplace') 
-        # employeeID, lastName, firstName, otherAuthors
-        pub_rel_info = parsePublicationRelationships(pub_rel_url)
-
-
-	# [employeeID, lastName, firstName, otherAuthors, title, source, startPage, year]
-        #do_inCites_publications(pub_info,pub_rel_info)	
-        print pub_url
-	print pub_info
-	#do_inCites_publications(["","","",pub_rel_info,title,"","",""])
+    #cate = e('//def:entry[1]/def:category[1]')[0].get('scheme')
+    #print '/def:feed/def:entry[1]/def:category[1]'
 
     # get next/last     
-    next = dom.getElementsByTagName('api:page')[2].getAttribute('href')
-    last = dom.getElementsByTagName('api:page')[3].getAttribute('href')
-    if next <> last:
-        parsePublicationList(next)
+    next = e('//api:page[@position="next"]')[0].get('href')
+    last = e('//api:page[@position="last"]')[0].get('href')
+    #if next <> last:
+    #    parsePublicationList(next)
 
 def parsePublicationUrl(xmlFile):
-    tree = etree.parse(xmlFile)
 
     file = urllib2.urlopen(xmlFile)
     data = file.read()
     file.close()
     dom = parseString(data)
 
+
+    return []
+
+
+def parsePublicationUrlXslt(xmlFile):
+    xml_input = etree.parse(xmlFile)
+    xslt_root = etree.parse("symplectic-to-vivo.datamap.xsl")
+    transform = etree.XSLT(xslt_root)
+    rdf_xml = transform(xml_input)
+    
+    dom = parseString(str(rdf_xml))
+
+    employeeID = ""
+    lastName = ""
+    firstName = ""
+    middleName = ""
+    otherauthors = ""
+    reftype = ""
+    title = getValue(dom,'core:Title')
+    sourcetitle = ""
+    volume = getValue(dom,'bibo:volume')
+    number = ""
+    startingpage = getValue(dom,'bibo:pageStart')
+    ISSN = getValue(dom,'bibo:ISSN')
+    dateday = ""
+    datemonth = ""
+    dateyear = ""
+    electronicresourcenum = getValue(dom,'bibo:doi')
+    accessionnum = ""
+    puburl = ""
+
+    list = [employeeID,lastName,firstName,middleName,otherauthors,reftype,title,sourcetitle,volume,number,startingpage,ISSN,dateday,datemonth,dateyear,electronicresourcenum,accessionnum,puburl]
+    do_inCites_publications(list)
+ 
+    return ""
+
+def getValue(dom,name):
     try:
-        entries = dom.getElementsByTagName('entry')[0].getElementsByTagName('api:object')[0].getElementsByTagName('api:records')[0].\
-                  getElementsByTagName('api:record')[0].getElementsByTagName('api:native')[0].getElementsByTagName('api:field')[6].getElementsByTagName('api:text')[0].childNodes[0].data
-    except Exception, err:
-        entries = []
+        return dom.getElementsByTagName(name)[0].childNodes[0].nodeValue
+    except:
+        return ""
 
 
-    return  str(entries)
-
-    #authors_list = []
-    #all_authors_string = ""
-    #for entry in entries:
-    #    authors = (entry.getElementsByTagName('api:person'))
-        #for author in authors:
-            #author_lastname = author.getElementsByTagName('api:last-name')[0].childNodes[0].data
-            #author_initials = author.getElementsByTagName('api:initials')[0].childNodes[0].data
-            #authorstring = author_lastname + ", " + author_initials 
-            #authors_list.append(authorstring)
-            #all_authors_string = all_authors_string + ";" + authorstring
-
-    #return set(authors_list)
-    #return all_authors_string[1:]
-
-    
-
-def parsePublicationRelationships(xmlFile):
-    tree = etree.parse(xmlFile)
-
-    file = urllib2.urlopen(xmlFile)
-    data = file.read()
-    file.close()
-    dom = parseString(data)
-
-    entries = dom.getElementsByTagName('entry')
-    
-
-    authors = []
-    # one entry per author, get name and employee id
-    for entry in entries:
-         authorname = ((entry.getElementsByTagName('title')[0].childNodes[0].data).split(": "))[1]
-         authorlink = ((entry.getElementsByTagName('link'))[2].getAttribute('href')).split("/")
-         authorid = authorlink[len(authorlink)-1]
-         this_author = [authorname, authorid]
-         authors.append(this_author)
-    #    for author in authors:
-    #        author_lastname = author.getElementsByTagName('api:last-name')[0].childNodes[0].data
-    #        author_initials = author.getElementsByTagName('api:initials')[0].childNodes[0].data
-    #        authorstring = author_lastname + ", " + author_initials
-    #        authors_list.append(authorstring)
-    #        all_authors_string = all_authors_string + ";" + authorstring
-    return authors
-
-# prints publications export for in_cites
 def do_inCites_publications(list):
-    #[employeeID, lastName, firstName, otherAuthors, title, source, startPage, year]
+    #[employeeID,lastName,firstName,middleName,other-authors,ref-type,title,source-title,volume,number,starting-page,ISSN,date-day,date-month,date-year,electronic-resource-num,accession-num,pub-url]
+
     employeeID = list.pop(0)
     lastName = list.pop(0)
     firstName = list.pop(0)
-    otherAuthors = list.pop(0)
+    middleName = list.pop(0)
+    otherauthors = list.pop(0)
+    reftype = list.pop(0)
     title = list.pop(0)
-    source = list.pop(0)
-    startPage = list.pop(0)
-    year = list.pop(0)
+    sourcetitle = list.pop(0)
+    volume = list.pop(0)
+    number =  list.pop(0)
+    startingpage = list.pop(0)
+    ISSN =  list.pop(0)
+    dateday = list.pop(0)
+    datemonth = list.pop(0)
+    dateyear = list.pop(0)
+    electronicresourcenum = list.pop(0)
+    accessionnum = list.pop(0)
+    puburl = list.pop(0)
+
     
-    print employeeID + "," + lastName + "," + firstName + "," + str(otherAuthors) + "," + title + "," + source + "," + startPage + "," + year
-
-
+    print employeeID + "," + lastName + "," + firstName + "," + middleName + "," + otherauthors + "," + reftype + "," + title + "," + sourcetitle + "," + volume + "," + startingpage + "," + ISSN + "," + dateday + "," + datemonth + "," + dateyear + "," + electronicresourcenum
 
 #read settings, load url, parse resulting text
 settings_text = open("config.json", "r").read()
