ff4f6527fa367ae5747a3da15b7577ee5ab245dd
Giuseppe Sollazzo <puntofisso@gmail.com>
2013-08-16 17:44:49 +0100

    Finished second round

--- site/menu.php
+++ site/menu.php
@@ -14,6 +14,7 @@
               <li><a href="sparql.php">SPARQL</a></li> 
               <li class="nav-header">Legal &amp; Info</li> 
               <li><a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/">OGL</a></li> 
+              <li><a href="contact.php">Contact</a></li>
             </ul> 
           </div><!--/.well --> 
         </div><!--/span-->
